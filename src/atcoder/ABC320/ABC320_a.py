b6b37a6a085c8c214ace06b7893d21514282754d
hiroyannnn <hiroyannnn@gmail.com>
2023-11-18 22:32:28 +0900

    ABC320

---
+++ src/atcoder/ABC320/ABC320_a.py
@@ -0,0 +1,10 @@
+# TODO edit this code, this code is for https://atcoder.jp/contests/practice/tasks/practice_1
+
+# param
+b, c = map(int, input().split())
+
+# solve
+ans = b ** c + c ** b
+
+# answer
+print("{}".format(ans))
